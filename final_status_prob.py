0c9f3b03ee5c4bddd5513eb36ae5b56528465551
dhruva-star88 <dhruvatej6565@gmail.com>
2024-11-16 12:20:29 +0530

    status prob

---
+++ final_status_prob.py
@@ -0,0 +1,7 @@
+def final_status(x):
+    status = x.split(";")
+    n = len(status)
+    return (print(status[n - 1]))
+
+
+final_status(x = "returned")
---
+++ tennis.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int tennis(int a){
+    for(int i=0; i< )
+}
+
+
+int main(){
+    int a[10];
+
+}
