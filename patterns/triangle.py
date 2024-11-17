09170e7e98b4e116fdd0c65e0c6d5313ecb2f59f
dhruva-star88 <dhruvatej6565@gmail.com>
2024-11-17 20:18:26 +0530

    triangle and reverse triangle

--- patterns/triangle.py
+++ patterns/triangle.py
@@ -1,10 +1,28 @@
 def triangle(n):
     c = 0
     for i in range(n):
-        c = c +  1
-        for j in range(c):
-            print("*", end='')
-
+        c = 2 * i + 1
+        for j in range(n - i - 1):
+            print(" ", end=" ")
+        for m in range(c):
+            print("*", end=" ")
+        for k in range(n - i - 1):
+            print(" ", end=" ")
         print('\n')
+    
 
 triangle(5)
+
+def rev_triangle(n):
+    c = 0
+    for i in range(n):
+        c = n - i - 1
+        for j in range(i):
+            print(" ", end=" ")
+        for k in range(2 * c + 1):
+            print("*", end=" ")
+        for l in range(i):
+            print(" ", end=" ")
+        print('\n')
+
+rev_triangle(5)
