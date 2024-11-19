42ec976c71e93f11da955b15645aab4cb413f00c
dhruva-star88 <dhruvatej6565@gmail.com>
2024-11-19 19:57:30 +0530

    diamond

--- patterns/diamond.py
+++ patterns/diamond.py
@@ -1,5 +1,23 @@
 def diamond(n):
-    n = n / 2
     c = 0
     for i in range(n):
-        for j in range(n): 
+        for j in range(n - i - 1):
+            print(" ", end=" ")
+        for k in range(2 * i + 1):
+            print("*", end=" ")
+        for l in range(n - i - 1):
+            print(" ", end=" ")
+        print("\n")
+    for m in range(n):
+        c = n - m - 1
+        for z in range(m):
+            print(" ", end=' ')
+        for o in range(2 * c + 1):
+            print("*", end=' ')
+        for p in range(m):
+            print(" ", end=' ')
+        print("\n")
+    
+
+diamond(6)
+            
