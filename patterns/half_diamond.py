7752d689084086ab80c76063309cf4526f564504
dhruva-star88 <dhruvatej6565@gmail.com>
2024-11-20 21:51:56 +0530

    some

--- patterns/half_diamond.py
+++ patterns/half_diamond.py
@@ -3,7 +3,7 @@ def half_diamond(n):
     for i in range(n):
         c = c + 1
         for j in range(c):
-            print("*", end="")
+            print("*", end=" ")
             print("\n")
     for k in range(n - 1):
         for m in range(c - 1):
