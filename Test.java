c1fbb417075b4e359ee487add3aac4da9da1ed54
dhruva-star88 <dhruvatej6565@gmail.com>
2024-11-24 19:47:22 +0530

    Java Commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class Patterns {
+    public Patterns() {
+    }
+ 
+    public static void main(String[] var0) throws Exception {
+       System.out.println("Dhruva");
+    }
+ }
+ 
