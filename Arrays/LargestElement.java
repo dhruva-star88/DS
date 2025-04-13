d206308646797a8c352c09e4a2a49ad6b7544576
Dhruva <dhruvatej6565@gmail.com>
2025-04-13 19:46:48 +0530

    Largest Array

---
+++ Arrays/LargestElement.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class LargestElement {
+    public static int LargestElement(int[] arr){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    } 
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Largest element in the array is: " + LargestElement(arr));
+    }
+}
